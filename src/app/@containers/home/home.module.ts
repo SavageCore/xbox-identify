99c385565fd153ea342b7ca8fc2d1fc64fb9b466
SavageCore <talk@savagecore.eu>
2021-02-01 03:25:59 +0000

    Remove unused typeof module

--- src/app/@containers/home/home.module.ts
+++ src/app/@containers/home/home.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { TypeofModule } from '@app/@core/pipes/typeof/typeof.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePage } from './home.page';
 
 @NgModule({
   declarations: [HomePage],
-  imports: [CommonModule, HomeRoutingModule, ReactiveFormsModule, TypeofModule],
+  imports: [CommonModule, HomeRoutingModule, ReactiveFormsModule],
 })
 export class HomeModule {}
