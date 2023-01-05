31440abbf7a28d9683c98f7a761a33bed67e204b
SavageCore <talk@savagecore.uk>
2023-01-05 06:04:27 +0000

    Remove i18n, how did that get there!

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -45,7 +45,6 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import '@angular/localize/init';
 import 'zone.js'; // Included with Angular CLI.
 
 /***************************************************************************************************
