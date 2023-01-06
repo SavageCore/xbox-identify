a10522d40ace3692c4d07b69496f43fd666ccd5e
SavageCore <talk@savagecore.uk>
2023-01-06 21:03:49 +0000

    Change settings icon colour based on theme

--- src/app/lib/components/navbar/navbar.component.ts
+++ src/app/lib/components/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { REPOSITORY_URL } from '@lib/constants';
-import { AuthService } from '@lib/services';
+import { AuthService, ThemeService } from '@lib/services';
+import { AppTheme } from '@lib/services/theme';
 import { LogoComponent } from '../logo/logo.component';
 
 @Component({
@@ -13,10 +14,23 @@ import { LogoComponent } from '../logo/logo.component';
   styleUrls: ['./navbar.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent {
-  readonly repositoryURL = REPOSITORY_URL;
+export class NavbarComponent implements OnInit {
+  public readonly repositoryURL = REPOSITORY_URL;
+  public currentTheme!: AppTheme | null;
 
-  constructor(private _router: Router, private _authService: AuthService) {}
+  constructor(
+    private _router: Router,
+    private _authService: AuthService,
+    private _themeService: ThemeService,
+    private _changeDetectorRef: ChangeDetectorRef,
+  ) {}
+
+  ngOnInit(): void {
+    this._themeService.currentTheme$.subscribe((theme) => {
+      this.currentTheme = theme;
+      this._changeDetectorRef.detectChanges();
+    });
+  }
 
   onClickSignOut(): void {
     this._authService.logout();
