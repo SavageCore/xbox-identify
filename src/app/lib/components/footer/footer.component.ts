7e371d283cc139af81b8d1d1aabb5cb52036724b
SavageCore <talk@savagecore.uk>
2023-01-06 18:42:45 +0000

    Link to my website in footer

--- src/app/lib/components/footer/footer.component.ts
+++ src/app/lib/components/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { REPOSITORY_URL } from '@lib/constants';
+import { AUTHOR_URL, REPOSITORY_URL } from '@lib/constants';
 import { LogoComponent } from '../logo/logo.component';
 
 @Component({
@@ -14,5 +14,5 @@ import { LogoComponent } from '../logo/logo.component';
 })
 export class FooterComponent {
   readonly repositoryURL = REPOSITORY_URL;
-  readonly currentYear = new Date().getFullYear();
+  readonly authorURL = AUTHOR_URL;
 }
--- src/app/lib/constants.ts
+++ src/app/lib/constants.ts
@@ -3,3 +3,4 @@ import { AppTheme } from './services/theme';
 export const DEFAULT_BASE_THEME: AppTheme = 'system' as const;
 
 export const REPOSITORY_URL: string = 'https://github.com/SavageCore/xbox-identify' as const;
+export const AUTHOR_URL: string = 'https://savagecore.uk' as const;
